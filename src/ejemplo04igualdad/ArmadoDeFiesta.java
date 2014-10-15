7f9f855c07eeae742345a1f66d8b2d8d9318d294
Santiago Della Torre <sdellatorre@gmail.com>
2014-10-15 19:07:33 -0300

    Se egrega ejemlo de la fiesta

--- src/ejemplo04igualdad/ArmadoDeFiesta.java
+++ src/ejemplo04igualdad/ArmadoDeFiesta.java
@@ -17,6 +17,7 @@ public class ArmadoDeFiesta {
 		fiesta.invitar( new Persona(34123127, "Ana Gomez",      sdf.parse("01/09/1989")));
 		
 		System.out.println("===INVITADOS===");
+		
 		for (Persona invitado: fiesta.getInvitados()){
 			if (invitado != null)
 				System.out.println(invitado);
